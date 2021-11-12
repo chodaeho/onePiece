93a8bea8c938f6eb65f4093b7fd22c0e08931e90
daehocho <daehocho0637@gmail.com>
2021-11-12 21:56:17 +0900

    table view 추가

---
+++ BountyList/BountyList/BountyViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BountyViewController.swift
+//  BountyList
+//
+//  Created by 조대호 on 2021/11/12.
+//
+
+import UIKit
+
+class BountyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
