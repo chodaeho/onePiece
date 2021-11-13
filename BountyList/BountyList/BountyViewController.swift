255fa71dbba0bf4e47c25421c938f2cd285253e8
daehocho <daehocho0637@gmail.com>
2021-11-13 10:47:30 +0900

    UITableViewDataSource, UITableViewDelegate 의 사용 방법

--- BountyList/BountyList/BountyViewController.swift
+++ BountyList/BountyList/BountyViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class BountyViewController: UIViewController {
+class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    
+    // UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    // UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("--> \(indexPath.row)")
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
