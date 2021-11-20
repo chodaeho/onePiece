a825a75fd1064a9c54e971952dfdab37c26f9a1f
daehocho <daehocho0637@gmail.com>
2021-11-20 14:29:20 +0900

    이미지 추가 및 오토레이아웃 수정

--- BountyList/BountyList/BountyViewController.swift
+++ BountyList/BountyList/BountyViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
+    let nameList = ["brook","chopper","franky","luffy","nami","robin","sanji","zoro"]
+    let bountyList = ["33000000","50","44000000","3000000000","16000000","80000000","77000000","1200000000"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,11 +19,18 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
     
     // UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return bountyList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+        }
+        
+        let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+        cell.imgView.image = img
+        cell.nameLabel.text = nameList[indexPath.row]
+        cell.bountyLable.text = "\(bountyList[indexPath.row])"
         
         return cell
     }
@@ -33,7 +43,7 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
 }
 
 class ListCell: UITableViewCell {
-    @IBOutlet weak var imaView: UIImageView!
+    @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLable: UILabel!
 }
