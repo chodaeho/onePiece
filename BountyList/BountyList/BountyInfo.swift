7354a9f18451cc3cccb1850f8634f2500cd45609
daehocho <daehocho0637@gmail.com>
2021-11-27 12:01:36 +0900

    bountyViewController에 연관없는 bountyInfo 따로 생성

---
+++ BountyList/BountyList/BountyInfo.swift
@@ -0,0 +1,22 @@
+//
+//  BountyInfo.swift
+//  BountyList
+//
+//  Created by 조대호 on 2021/11/27.
+//
+
+import UIKit
+
+struct BountyInfo {
+    let name: String
+    let bounty: Int
+    
+    var image: UIImage? {
+        return UIImage(named: "\(name).jpg")
+    }
+    
+    init(name: String, bounty: Int) {
+        self.name = name
+        self.bounty = bounty
+    }
+}
