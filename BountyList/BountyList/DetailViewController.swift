1437fa9f75437a3dfc36e42650f5d8dd6e9762b7
daehocho <daehocho0637@gmail.com>
2021-11-27 20:28:59 +0900

    Label Animation 적용

--- BountyList/BountyList/DetailViewController.swift
+++ BountyList/BountyList/DetailViewController.swift
@@ -5,72 +5,40 @@
 //  Created by 조대호 on 2021/11/21.
 //
 //
-//import UIKit
-//
-//class DetailViewController: UIViewController {
-//
-//
-//
-//    @IBOutlet weak var imgView: UIImageView!
-//    @IBOutlet weak var nameLabel: UILabel!
-//    @IBOutlet weak var bountyLabel: UILabel!
-//
-//    var name: String?
-//    var bounty: String?
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        updateUI()
-//    }
-//
-//    func updateUI() {
-//        if let name = self.name, let bounty = self.bounty {
-//            let img = UIImage(named: "\(name).jpg")
-//            imgView.image = img
-//            nameLabel.text = name
-//            bountyLabel.text = bounty
-//        }
-//    }
-//
-//    @IBAction func close(_ sender: Any) {
-//        dismiss(animated: true, completion: nil)
-//    }
-//
-//}
-
-// MVVM 디자인패턴으로 변경
 
 import UIKit
 
 class DetailViewController: UIViewController {
-    
-//    MVVM 디자인 패턴으로 변경하기
-    
-//    Model
-//    - BountyInfo
-//    >BountyInfo 생성
-    
-//    View
-//    - imgView, nameLable, bountyLable
-//    > view들은 viewModel을 통해서 구성
-    
-//    ViewModel
-//    - DetailViewModel
-//    > 뷰레이어에서 필요한 메서드 만들기
-//    > Model(BountyInfo)을 가지고 있어야 함.
-    
-    
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
+    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
+    @IBOutlet weak var bountyLabelCenterX: NSLayoutConstraint!
     
-    // MVVM 디자인패턴 ViewModel 이용
-//    var bountyInfo: BountyInfo?
     let viewModel = DetailViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        prepareAnimation()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        showAnimation()
+    }
+    
+    private func prepareAnimation() {
+        nameLabelCenterX.constant = view.bounds.width
+        bountyLabelCenterX.constant = view.bounds.width
+    }
+    
+    private func showAnimation() {
+        nameLabelCenterX.constant = 0
+        bountyLabelCenterX.constant = 0
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
     }
     
     func updateUI() {
@@ -87,7 +55,6 @@ class DetailViewController: UIViewController {
 
 }
 
-// MVVM 디자인패턴 ViewModel로 생성
 class DetailViewModel {
     var bountyInfo: BountyInfo?
     
